5c7ad40063761796529310549c4e3acc84d663fd
Oğuzhan SARI <oguzn.sari@gmail.com>
2024-01-04 21:41:21 +0300

    Two Factor authentication 2FA | local setup - no provider solution

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import authConfig from "@/auth.config";
 import { db } from "@/lib/db";
 import { getUserById } from "@/data/user";
 import { UserRole } from "@prisma/client";
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
 
 type ExtendedUser = DefaultSession["user"] & {
   role: "ADMIN" | "USER";
@@ -43,7 +44,18 @@ export const {
       // Prevent sign in without email verification
       if (!existingUser || !existingUser.emailVerified) return false;
 
-      // TODO: Add 2FA check
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        );
+
+        if (!twoFactorConfirmation) return false;
+
+        // Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: { id: twoFactorConfirmation.id },
+        });
+      }
 
       return true;
     },
